820805528842893d3ccf5435d1f328c364901515
Marcia Villalba <marciavillalba@Marcias-MacBook-Pro-2.local>
2019-06-07 16:18:43 +0300

    adding thundra

--- handler.js
+++ handler.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const thundra = require('@thundra/core')({ apiKey: process.env.THUNDRA_KEY });
+
 const uuidv1 = require('uuid/v1');
 const AWS = require('aws-sdk');
 
@@ -8,98 +10,108 @@ const orderMetadataManager = require('./orderMetadataManager');
 var sqs = new AWS.SQS({ region: process.env.REGION });
 const QUEUE_URL = process.env.PENDING_ORDER_QUEUE;
 
-module.exports.hacerPedido = (event, context, callback) => {
-	console.log('HacerPedido fue llamada');
+module.exports.hacerPedido = thundra((event, context, callback) => {
+    console.log('HacerPedido fue llamada');
 
-	const body = JSON.parse(event.body);
+    const body = JSON.parse(event.body);
 
-	const order = {
-		orderId: uuidv1(),
-		name: body.name,
-		address: body.address,
-		pizzas: body.pizzas,
-		timestamp: Date.now()
-	};
+    const order = {
+        orderId: uuidv1(),
+        name: body.name,
+        address: body.address,
+        pizzas: body.pizzas,
+        timestamp: Date.now()
+    };
 
-	const params = {
-		MessageBody: JSON.stringify(order),
-		QueueUrl: QUEUE_URL
-	};
+    const params = {
+        MessageBody: JSON.stringify(order),
+        QueueUrl: QUEUE_URL
+    };
 
-	sqs.sendMessage(params, function(err, data) {
-		if (err) {
-			sendResponse(500, err, callback);
-		} else {
-			const message = {
-				order: order,
-				messageId: data.MessageId
-			};
-			sendResponse(200, message, callback);
-		}
-	});
-};
+    sqs.sendMessage(params, function(err, data) {
+        if (err) {
+            sendResponse(500, err, callback);
+        } else {
+            const message = {
+                order: order,
+                messageId: data.MessageId
+            };
+            sendResponse(200, message, callback);
+        }
+    });
+});
 
-module.exports.prepararPedido = (event, context, callback) => {
-	console.log('Preparar pedido fue llamada');
+module.exports.prepararPedido = thundra((event, context, callback) => {
+    console.log('Preparar pedido fue llamada');
 
-	const order = JSON.parse(event.Records[0].body);
+    const order = JSON.parse(event.Records[0].body);
 
-	orderMetadataManager
-		.saveCompletedOrder(order)
-		.then(data => {
-			callback();
-		})
-		.catch(error => {
-			callback(error);
-		});
-};
+    orderMetadataManager
+        .saveCompletedOrder(order)
+        .then(data => {
+            callback();
+        })
+        .catch(error => {
+            callback(error);
+        });
+});
 
-module.exports.enviarPedido = (event, context, callback) => {
-	console.log('enviarPedido fue llamada');
+module.exports.enviarPedido = thundra((event, context, callback) => {
+    console.log('enviarPedido fue llamada');
 
-	const record = event.Records[0];
-	if (record.eventName === 'INSERT') {
-		console.log('deliverOrder');
+    const record = event.Records[0];
+    if (record.eventName === 'INSERT') {
+        console.log('deliverOrder');
 
-		const orderId = record.dynamodb.Keys.orderId.S;
+        const orderId = record.dynamodb.Keys.orderId.S;
 
-		orderMetadataManager
-			.deliverOrder(orderId)
-			.then(data => {
-				console.log(data);
-				callback();
-			})
-			.catch(error => {
-				callback(error);
-			});
-	} else {
-		console.log('is not a new record');
-		callback();
-	}
-};
+        orderMetadataManager
+            .deliverOrder(orderId)
+            .then(data => {
+                console.log(data);
+                callback();
+            })
+            .catch(error => {
+                callback(error);
+            });
+    } else {
+        console.log('is not a new record');
+        callback();
+    }
+});
 
-module.exports.estadoPedido = (event, context, callback) => {
-	console.log('Estado pedido fue llamado');
+module.exports.estadoPedido = thundra((event, context, callback) => {
+    console.log('Estado pedido fue llamado');
 
-	const orderId = event.pathParameters && event.pathParameters.orderId;
-	if (orderId !== null) {
-		orderMetadataManager
-			.getOrder(orderId)
-			.then(order => {
-				sendResponse(200, `El estado de la orden: ${orderId} es ${order.delivery_status}`, callback);
-			})
-			.catch(error => {
-				sendResponse(500, 'Hubo un error al procesar el pedido', callback);
-			});
-	} else {
-		sendResponse(400, 'Falta el orderId', callback);
-	}
-};
+    const orderId = event.pathParameters && event.pathParameters.orderId;
+    if (orderId !== null) {
+        orderMetadataManager
+            .getOrder(orderId)
+            .then(order => {
+                sendResponse(
+                    200,
+                    `El estado de la orden: ${orderId} es ${
+                        order.delivery_status
+                    }`,
+                    callback
+                );
+            })
+            .catch(error => {
+                sendResponse(
+                    500,
+                    'Hubo un error al procesar el pedido',
+                    callback
+                );
+            });
+    } else {
+        sendResponse(400, 'Falta el orderId', callback);
+    }
+});
 
 function sendResponse(statusCode, message, callback) {
-	const response = {
-		statusCode: statusCode,
-		body: JSON.stringify(message)
-	};
-	callback(null, response);
+    const response = {
+        statusCode: statusCode,
+        body: JSON.stringify(message)
+    };
+    callback(null, response);
 }
